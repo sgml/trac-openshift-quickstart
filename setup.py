43521732e6a5ecacfa6c58e72a723257e50bcfbe
Kelvin Mo <kmo@users.sourceforge.net>
2013-07-11 23:51:59 +1000

    Added AccountManager plugin

--- setup.py
+++ setup.py
@@ -7,13 +7,23 @@ setup(
     author='Kelvin Mo',
 
     install_requires=[
+        # The web server
+        'greenlet',
         'gevent',
+
+        # Trac and various dependencies
         'Trac == 1.0',
         'MySQL-python',
         'psycopg2',
         'Babel',
         'docutils',
         'Pygments',
-        'pytz'
+        'pytz',
+
+        # Some useful plugins
+        'TracAccountManager',
+
+        # Add your trac plugins here
+
     ],
 )
